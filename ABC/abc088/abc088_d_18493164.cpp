f1bd6447c7ba2e4f241a75c1cca27efe5c0f73f8
takase <taka.shun.2018@gmail.com>
2020-12-01 00:00:13 +0900

    202012010000

---
+++ ABC/abc088/abc088_d_18493164.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define PI 3.14159265358979323846
+#define MAXINF (1e18L)
+#define INF (1e9L)
+#define EPS (1e-9)
+#define MOD ((ll)(1e9+7))
+#define REP(i, n) for(int i=0;i<int(n);++i)
+#define REPA(x, v) for(auto&& x:v)
+#define Rep(i,sta,n) for(int i=sta;i<n;i++)
+#define RREP(i, n) for(int i=int(n)-1;i>=0;--i)
+#define ALL(v) v.begin(),v.end()
+#define FIND(v,x) (binary_search(ALL(v),(x)))
+#define SORT(v) sort(ALL(v))
+#define RSORT(v) sort(v.rbegin(),v.rend())
+#define DEBUG(x) cerr<<#x<<": "<<x<<endl;
+#define DEBUG_VEC(v) cerr<<#v<<":";for(int i=0;i<v.size();i++) cerr<<" "<<v[i]; cerr<<endl
+#define Yes(n) cout<<((n)?"Yes":"No")<<endl
+#define YES(n) cout<<((n)?"YES":"NO")<<endl
+#define pb push_back
+#define fi first
+#define se second
+using namespace std;
+template<class A>void pr(A a){cout << (a) << endl;}
+template<class A,class B>void pr(A a,B b){cout << a << " "  ;pr(b);}
+template<class A,class B,class C>void pr(A a,B b,C c){cout << a << " " ;pr(b,c);}
+template<class A,class B,class C,class D>void pr(A a,B b,C c,D d){cout << a << " " ;pr(b,c,d);}
+template<class T> void pr_vec(vector<T> &v, char ep=' '){int n=v.size();REP(ni,n){cout<<v[ni];if(ni!=n-1)cout<<ep;}cout<<endl;};
+template<class T> inline bool chmin(T& a, T b){return a>b ? a=b, true : false;}
+template<class T> inline bool chmax(T& a, T b){return a<b ? a=b, true : false;}
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+
+int main(void)
+{
+    int H,W;
+    cin >> H >> W;
+    vector<string> s(H);
+    REP(i,H) cin >> s[i];
+    
+    int sp_num = 0;
+    REP(i, H)REP(j, W){
+        if(s[i][j]=='#') sp_num++;
+    }
+
+    queue<pii> q;
+    q.push({0,0});
+    vector<vector<bool>> vis(H, vector<bool>(W, false));
+    vector<vector<int>> dist(H, vector<int>(W, 0));
+    vis[0][0] = true;
+    dist[0][0] = 1;
+    while (!q.empty())
+    {
+        auto f = q.front();q.pop();
+        if(f.fi==H-1&&f.se==W-1) break;
+        int dx[4]={-1,1,0,0}, dy[4]={0,0,-1,1};
+        REP(i, 4){
+            int x = f.fi + dx[i];
+            int y = f.se + dy[i];
+            if(0<=x && x<H && 0<=y && y<W && s[x][y]!='#' && !vis[x][y]){
+                vis[x][y] = true;
+                dist[x][y] = dist[f.fi][f.se] + 1;
+                q.push({x,y});
+            }
+        }
+    }
+
+    if(vis[H-1][W-1]==false)pr(-1);
+    else pr(H*W-sp_num-dist[H-1][W-1]);   
+}
